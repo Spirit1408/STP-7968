ebb14863993cd82a9a2d0962b7ce148f05a0b128
Oleh Shalashov <spirit14081992@gmail.com>
2025-02-18 19:13:32 -0300

    Update styles fix

--- global.styles.js
+++ global.styles.js
@@ -1,10 +1,8 @@
-const variablesStyles = '@import "./src/scss/common/_vars.scss"';
 const breakpointsStyles = '@import "./src/scss/common/_breakpoints.scss"';
 const mixinsStyles = '@import "./src/scss/common/_mixins.scss"';
 const resetStyles = '@import "./src/scss/common/_reset.scss"';
 
 const styles = [
-  variablesStyles,
   breakpointsStyles,
   //   colorsStyles,
   mixinsStyles,
