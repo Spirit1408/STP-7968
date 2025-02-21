09ca4201d3fa75addf79d3e9f4e47f9b8fe1e307
Mariya Buzhava <bujava.masha@gmail.com>
2025-02-21 10:38:28 +0200

    add header

---
+++ src/js/header.js
@@ -0,0 +1,26 @@
+export function setupBurgerMenu() {
+  const burgerOpen = document.querySelector('.burger-open');
+  const burgerClose = document.querySelector('.burger-close');
+  const mobileMenuWrapper = document.querySelector('.mobile-menu-wrapper');
+  const overlay = document.querySelector('.overlay');
+
+  if (burgerOpen && burgerClose && mobileMenuWrapper && overlay) {
+    function openMenu() {
+      mobileMenuWrapper.classList.add('active');
+      overlay.classList.add('active');
+      burgerOpen.style.display = 'none';
+      document.body.style.overflow = 'hidden';
+    }
+
+    function closeMenu() {
+      mobileMenuWrapper.classList.remove('active');
+      overlay.classList.remove('active');
+      burgerOpen.style.display = '';
+      document.body.style.overflow = '';
+    }
+
+    burgerOpen.addEventListener('click', openMenu);
+    burgerClose.addEventListener('click', closeMenu);
+    overlay.addEventListener('click', closeMenu);
+  }
+}
